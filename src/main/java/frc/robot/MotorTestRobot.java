8c7099c94e354334181cc11db5674a98eaf1fa92
Kay <kay.kasemir@gmail.com>
2023-01-06 14:53:28 -0500

    Falcon test

--- src/main/java/frc/robot/MotorTestRobot.java
+++ src/main/java/frc/robot/MotorTestRobot.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenixpro.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.parts.SparkMini;
@@ -13,7 +16,23 @@ import frc.robot.parts.SparkMini;
 public class MotorTestRobot extends TimedRobot
 {
   private final XboxController joystick = new XboxController(0);
-  private final SparkMini motor = new SparkMini(0);
+
+  // CTRE now has two APIs:
+  // The new "PhoenixPro" com.ctre.phoenixpro.hardware.TalonFX
+  // for C:\Users\Public\Documents\FRC\2023 Firmware\PhoenixPro\TalonFX-Application-23.4.0.3-Pro-season2023.crf
+  // which complains at runtime with
+  // "Device is not licensed. Cannot get any data from it."
+
+  // private final TalonFX motor= new TalonFX(3+1);
+  
+  // .. and the older "Phoenix 5"
+  // com.ctre.phoenix.motorcontrol.can.TalonFX with wrapper WPI_TalonFX
+  // that runs with 
+  // C:\Users\Public\Documents\FRC\2023 Firmware\Phoenix\TalonFX-Application-22.1.1.0-season2023.crf
+  private final WPI_TalonFX motor = new WPI_TalonFX(3+1);
+
+  // Simple PWM controller:
+  // private final SparkMini motor = new SparkMini(0);
 
   @Override
   public void robotInit()

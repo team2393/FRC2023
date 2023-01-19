8720769a24477ab31d9c7ff952f6dd35a2b6d49e
Kay <kay.kasemir@gmail.com>
2023-01-18 20:56:09 -0500

    REV through bore encoder

--- src/main/java/frc/robot/ThroughBoreEncoderRobot.java
+++ src/main/java/frc/robot/ThroughBoreEncoderRobot.java
@@ -2,18 +2,27 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycle;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /** REV Through-Bore Encoder demo robot */
 public class ThroughBoreEncoderRobot extends CommandBaseRobot
 {
   // Absolute readout uses (white, red, black) into DI
-  private DutyCycle encoder = new DutyCycle(new DigitalInput(1));
+  private DutyCycleEncoder encoder = new DutyCycleEncoder(new DigitalInput(1));
   
+  @Override
+  public void robotInit()
+  {
+    super.robotInit();
+    encoder.reset();
+  }
+
   @Override
   public void robotPeriodic()
   {
     super.robotPeriodic();
-    SmartDashboard.putNumber("Position", encoder.getHighTimeNanoseconds());
+    // Encoder returns value 0...1, scale to 0..360 degrees
+    SmartDashboard.putNumber("Position", encoder.getAbsolutePosition() * 360.0);
   }
 }

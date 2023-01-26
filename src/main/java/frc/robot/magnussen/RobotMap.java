8a458efeb745bd22e51ce7a3a876b1d02fe0589a
Kay Kasemir <kay.kasemir@gmail.com>
2023-01-26 11:45:28 -0500

    Update RobotMap.java

--- src/main/java/frc/robot/magnussen/RobotMap.java
+++ src/main/java/frc/robot/magnussen/RobotMap.java
@@ -6,13 +6,14 @@ package frc.robot.magnussen;
 /** Map of all inputs and outputs: CAN IDs etc. */
 public class RobotMap
 {
-  /** Power Distribution Module        TODO Check actual fuse assignments
-   * 
+  /** Power Distribution Module
+   *
    *  See https://docs.wpilib.org/en/stable/docs/controls-overviews/control-system-hardware.html
-   * 
+   *
+   *  TODO Check actual fuse assignments
    *  REV Power Distribution Hub
-   *  10 - 20 Amp front left rotator           9 - 40 Amp front left driver
-   *  11 -                                     8 - 40 Amp front right driver
+   *  10 - 20 Amp front left rotator?          9 - 40 Amp front left driver?
+   *  11 -                                     8 - 40 Amp front right driver?
    *  12 -                                     7 - 
    *  13 -                                     6 - 
    *  14 -                                     5 - 
@@ -20,12 +21,16 @@ public class RobotMap
    *  16 -                                     3 - 
    *  17 -                                     2 - 
    *  18 -                                     1 - 
-   *  19 - 20 Amp Pneumatic hub -> Compressor  0 - 
+   *  19 - 20 Amp Pneumatic hub -> Compressor? 0 - 
    *  20 - 10 Amp Radio power
    *  21 - 10 Amp RoboRIO
    *  22 - 10 Amp CANivore (optional?)
    *  23 - 10 Amp Camera LED ring (switched)
+   *
+   *  ^^^^  24 ports (0-23) ^^^^^
    * 
+   *  23 devices to be powered:
+   *
    *  40 Amp Front left driver
    *  40 Amp Front right driver
    *  40 Amp Back right driver
@@ -41,6 +46,8 @@ public class RobotMap
    *  20 Amp Back left rotator
    *  20 Amp Pneumatic hub -> Compressor
    * 
+   *  10 Amp RoboRIO
+   *  10 Amp Radio power
    *  10 Amp CANivore (optional?)
    *  10 Amp Front left CANcoder
    *  10 Amp Front right CANcoder

2621c7c5d29f12b6f09b87b78e5992df1873d0e3
Kay <kay.kasemir@gmail.com>
2023-02-08 07:45:15 -0500

    Use states?

--- src/main/java/frc/robot/magnussen/TheGreatCoordinator.java
+++ src/main/java/frc/robot/magnussen/TheGreatCoordinator.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.magnussen;
 
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** The great arm/lift/grabber/intake coordinator
@@ -18,11 +19,25 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 public class TheGreatCoordinator extends SubsystemBase
 {
+  // Devices that we handle
   private final Lift lift = new Lift();
   private final Arm arm = new Arm();
   // private final Intake intake = new Intake();
 
-  private boolean lift_homed = false;
+  enum State
+  {
+    /** Prepare to home lift by moving it slightly 'up' off the switch */
+    PREP_LIFT,
+    /** Home lift */
+    HOME_LIFT,
+    /** Move arm etc. out of the way to provide intake with a clear path */
+    CLEAR_INTAKE,
+    /** Everything can move */
+    NORMAL
+  }
+
+  private State state = State.PREP_LIFT;
+
   private double lift_height = 0.1;
   private double arm_angle = 0.0;
   private double intake_angle = 0.0;
@@ -48,41 +63,39 @@ public class TheGreatCoordinator extends SubsystemBase
   @Override
   public void periodic()
   {
-    // If lift hasn't been homed, do that
-    if (! lift_homed)
+    if (RobotState.isDisabled())
+      handleDisabled();
+    else
+      handleEnabled();
+  }
+
+  private void handleDisabled()
+  {
+    // No motors are moving.
+    // Once we re-enable, start over by homing the lift? 
+    state = State.PREP_LIFT;
+  }
+
+  private void handleEnabled()
+  {
+    if (state == State.PREP_LIFT)
     {
-      // TODO Before moving the lift down, do arm and intake
-      // need to be in a certain position?
-      lift_homed = lift.home();
+
+    }
+    if (state == State.HOME_LIFT)
+    {
       arm.setAngle(-90);
       // intake.setAngle(45);
+      if (lift.home())
+        state = State.NORMAL;
     }
-    else
+    if (state == State.NORMAL)
     {
-      // Lift has been homed, now keep at desired position
-      lift.setHeight(lift_height);
-      /*
-      if (intake_angle > 90)
-      {
-        // To pull intake in, arm needs to be out of the way
-        arm.setAngle(-100.0);
-        if (arm.getAngle() < 90)
-        {
-          // Once arm is stored, we can move intake to desired location
-          intake.setAngle(intake_angle);
-        }
-        else
-        {
-          // Keep intake just before 90 until arm has been stored
-          intake.setAngle(85);
-        }
-      }
-      else
-      { // Intake is 'out', so arm can move as desired
-        arm.setAngle(arm_angle);
-        intake.setAngle(intake_angle);
-      }
-      */
+
+    }
+    if (state == State.CLEAR_INTAKE)
+    {
+
     }
   }
 }
